30daf0f76e099d123b07d1e6ed6721b0bd14510d
Brian Hoy <epitaquee@gmail.com>
2018-03-11 22:54:37 -0400

    Update about.component.ts

--- src/components/about/about.component.ts
+++ src/components/about/about.component.ts
@@ -3,8 +3,7 @@ import {Component} from '@angular/core';
 @Component({
 	selector: 'about',
 	template: `<img class="profile" src="/public/images/close-portrait.png">
-<p>Hi, I'm Brian, a junior in high school with a passion for developing websites. I'm currently interested in making a realistic procedural generated terrain in a web browser. If you'd like to hire me to create a website, feel free to contact me and I'll let you know if I think I can do it. </p>
-<p>I'm best at creating websites using the Angular 2 framework and Three.JS, but I can create basic backends with user authentication.`,
+<p>Hi, I'm Brian, a senior in high school with a passion for developing websites as well as Computer Graphics. I'm currently interested in making a realistic procedural generated terrain.`,
 	styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
